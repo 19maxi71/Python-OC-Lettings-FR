be2c6c836d54a19e0c45920a2d6015a672a8a406
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2025-03-13 01:03:49 +0100

    Registering new apps and urls. Moving templates to corresponding folders.

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from .models import Letting, Profile
+from .models import Profile
 
-def profiles_index(request):
+
+def index(request):
+    """
+    Displays the list of user profiles.
+    """
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
-# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis fringilla, eros leo tristique lacus,
-# it. Nam aliquam dignissim congue. Pellentesque habitant morbi tristique senectus et netus et males
+
 def profile(request, username):
+    """
+    Displays the details of a specific user profile.
+    """
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
