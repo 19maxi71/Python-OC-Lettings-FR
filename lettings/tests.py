88e255c2866d444d9efa421a9bfccd776d2a0ea5
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2025-03-14 13:57:11 +0100

    adding tests to lettings

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Address, Letting
 
-# Create your tests here.
+class LettingTest(TestCase):
+    def setUp(self):
+        self.address = Address.objects.create(
+            number=1, 
+            street='Test Street',
+            city='Test City',
+            state='TS',
+            zip_code=12345,
+            country_iso_code='US'
+        )
+        self.letting = Letting.objects.create(
+            title='Test Letting',
+            address=self.address
+        )
+
+    def test_lettings_index(self):
+        url = reverse('lettings:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Lettings')
+        self.assertContains(response, 'Test Letting')
+
+    def test_letting_detail(self):
+        url = reverse('lettings:letting', kwargs={'letting_id': self.letting.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Letting')
+        self.assertContains(response, 'Test Street')
