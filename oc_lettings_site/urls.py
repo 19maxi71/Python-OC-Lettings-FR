ecae2693db8e609d368796f7a2c50c5bdd57f41b
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2025-03-15 11:32:45 +0100

    changes to urls

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
     path('sentry-debug/', views.trigger_error, name='trigger_error'),  # For testing Sentry
-    path('test-404/', views.test_404, name='test_404'),  # For testing 404 template
-    path('test-500/', views.test_500, name='test_500'),  # For testing 500 template
+    path('test-404/', views.test_404, name='test-404'),  # For testing 404 template
+    path('test-500/', views.test_500, name='test-500'),  # For testing 500 template
 ]
